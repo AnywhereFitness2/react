66b3279d238dea43034d3b5757879655a6c3f43c
Your Real Name Here <Your Email Address Here>
2020-01-09 19:34:55 -0700

    ClassForm rendered. Styling started

--- src/components/ClassForm.js
+++ src/components/ClassForm.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
+import styled, {css} from 'styled-components';
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: center;
+border: 1px solid black;
+`;
+const Label = styled.label`
+text-align: left;
+`;
 export default function ClassForm () {
     const [classtributes, setClasstributes]= useState({className: '', classType:'', startTime: '', duration: '', intensityLevel: '', location: '', numberOfAttendees: '', maxClassSize: ''});
     const handleChanges = event => {
@@ -7,77 +17,94 @@ export default function ClassForm () {
         console.log(classtributes);
     }; 
     const onSubmit = event =>  {
-        event.preventDefault();
-        // props.addTeamMember(form);
+        event.preventDefault();        
         setClasstributes({className: '', classType:'', startTime: '', duration: '', intensityLevel: '', location: '', numberOfAttendees: '', maxClassSize: ''})
     }
     return (
-        <form onSubmit={onSubmit}>
-            <label>Name</label>
-            <input                
-                type='text' 
-                placeholder= 'enter name' 
-                onChange= {handleChanges}
-                name="name"
-                value= {classtributes.name}
+        <Form onSubmit={onSubmit}>
+            <div className= 'name'  >
+                <Label>Name: </Label>
+                <input                
+                    type='text' 
+                    placeholder= 'enter name' 
+                    onChange= {handleChanges}
+                    name="name"
+                    value= {classtributes.name}
+                    />
+            </div>
+            <div className= 'type'>
+                <Label>Type: </Label>
+                <input                 
+                    type='text'
+                    placeholder= 'Class Type'
+                    onChange= {handleChanges}
+                    name= 'classType'
+                    value= {classtributes.classType}
+                    />
+            </div>
+            <div className= 'startTime'>
+                <label>Start Time: </label>
+                <input             
+                    type='text'
+                    placeholder= 'Start Time'    
+                    onChange= {handleChanges}
+                    name= 'startTime'
+                    value= {classtributes.startTime}
                 />
-            <label>Class Type</label>
-            <input                 
-                type='text'
-                placeholder= 'Class Type'
-                onChange= {handleChanges}
-                name= 'classType'
-                value= {classtributes.classType}
+            </div>
+            <div className= 'duration'>
+                <label>Duration: </label>
+                <input              
+                    type='text'
+                    placeholder= 'Duration'    
+                    onChange= {handleChanges}
+                    name= 'duration'
+                    value= {classtributes.duration}
                 />
-            <label>Start Time</label>
-            <input                 
-                type='text'
-                placeholder= 'Start Time'    
-                onChange= {handleChanges}
-                name= 'startTime'
-                value= {classtributes.startTime}
-            />
-            <label>Duration</label>
-            <input                 
-                type='text'
-                placeholder= 'Duration'    
-                onChange= {handleChanges}
-                name= 'duration'
-                value= {classtributes.duration}
-            />
-            <label>Location</label>
-            <input                 
-                type='text'
-                placeholder= 'Location'    
-                onChange= {handleChanges}
-                name= 'location'
-                value= {classtributes.location}
-            />
-            <label>Current Number of Registered Attendees</label>
-            <input                 
-                type='text'
-                placeholder= 'Number of  Attendees'    
-                onChange= {handleChanges}
-                name= 'numberOfAttendees'
-                value= {classtributes.currentNumberOfAttendees}
-            />
-             <label>Max Class Size</label>
-            <input                 
-                type='text'
-                placeholder= 'Max Class Size'    
-                onChange= {handleChanges}
-                name= 'maxClassSize'
-                value= {classtributes.maxClassSize}
-            /> <label>Intensity Level</label>
-            <input                 
-                type='text'
-                placeholder= 'intensityLevel'    
-                onChange= {handleChanges}
-                name= 'intensityLevel'
-                value= {classtributes.intensityLevel}
-            />
-            
+            </div>
+            <div>
+                <label>Location: </label>
+                <input               
+                    type='text'
+                    placeholder= 'Location'    
+                    onChange= {handleChanges}
+                    name= 'location'
+                    value= {classtributes.location}
+                />
+            </div>
+            <div>
+                <label>Attendees: </label>
+                <input                 
+                    type='text'
+                    placeholder= 'Number of  Attendees'    
+                    onChange= {handleChanges}
+                    name= 'numberOfAttendees'
+                    value= {classtributes.currentNumberOfAttendees}
+                />
+            </div>
+            <div>
+                <label>Max Class Size: </label>
+                <input                 
+                    type='text'
+                    placeholder= 'Max Class Size'    
+                    onChange= {handleChanges}
+                    name= 'maxClassSize'
+                    value= {classtributes.maxClassSize}
+                /> 
+                </div>
+            <div>
+                <label>Intensity: </label>
+                <input                 
+                    type='text'
+                    placeholder= 'Intensity Level'    
+                    onChange= {handleChanges}
+                    name= 'intensityLevel'
+                    value= {classtributes.intensityLevel}
+                />
+            </div>
+            <div>
             <button className= "submitButton" type= "submit">Click Here to Submit</button>
-        </form>
+            </div>
+        </Form>
             )
         }
